b5d7f30ef0b267589fdef34e02bb31e25c3a2369
moneer95 <mnyrskyk@gmail.com>
2025-02-08 02:29:15 +0300

    photos

--- lib/data/photography.ts
+++ lib/data/photography.ts
@@ -13,10 +13,11 @@ export const photographers: Artist[] = [
         price: 175,
         sizes: ["12\"x16\"", "16\"x20\"", "20\"x24\"", "24\"x36\""],
         images: [
-          "https://images.unsplash.com/photo-1469817805249-72b7df1c3c05?w=1200&q=80",
-          "https://images.unsplash.com/photo-1465447142348-e9952c393450?w=1200&q=80",
-          "https://images.unsplash.com/photo-1514565131-fce0801e5785?w=1200&q=80",
-          "https://images.unsplash.com/photo-1524645343120-a4ae9f7d4343?w=1200&q=80"
+            "/shop-photos/olive-women.jpg", 
+            "/shop-photos/gaza-destroyed.jpg", 
+            "/shop-photos/kids-wheel.jpg", 
+            "/shop-photos/night-light.jpg", 
+            "/shop-photos/olive-picking.jpg", 
         ]
       }
     ]
