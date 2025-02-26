2ed49690e45e6eefb95726efe6bbd0faaf5cb0ff
moneer95 <mnyrskyk@gmail.com>
2025-02-26 19:16:27 +0300

    oay

--- src/app/api/capture/route.js
+++ src/app/api/capture/route.js
@@ -4,18 +4,22 @@ import paypal from "@paypal/checkout-server-sdk";
 const clientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;
 const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
 
-
-// Configure PayPal environment
 const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
 const client = new paypal.core.PayPalHttpClient(environment);
 
 export async function POST(request) {
   try {
     const { orderID } = await request.json();
+    
+    if (!orderID) {
+      return NextResponse.json({ error: "Missing order ID" }, { status: 400 });
+    }
+
     const captureRequest = new paypal.orders.OrdersCaptureRequest(orderID);
     captureRequest.requestBody({});
 
     const response = await client.execute(captureRequest);
+    
     return NextResponse.json(response.result);
   } catch (error) {
     console.error("Error capturing PayPal payment:", error);
