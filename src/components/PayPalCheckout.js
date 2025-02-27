bfee283243e9355246fdbf8977cc6b9856c546d3
moneer95 <mnyrskyk@gmail.com>
2025-02-27 17:45:48 +0300

    1111

--- src/components/PayPalCheckout.js
+++ src/components/PayPalCheckout.js
@@ -35,6 +35,7 @@ export default function PayPalCheckout({ amount }) {
             color: "silver",
           }}
           createOrder={async (data, actions) => {
+            console.log('111')
             try {
               const res = await fetch("/api/paypal_checkout", {
                 method: "POST",
